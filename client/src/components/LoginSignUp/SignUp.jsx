8764120e8118c3708ab326e9bd314dcce5055b88
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-28 18:09:04 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/SignUp.jsx
+++ client/src/components/LoginSignUp/SignUp.jsx
@@ -1,6 +1,6 @@
 const SignUp = () => {
   return (
-    <div className="bg-[url()]">
+    <div className="bg-[url(https://wallpapercave.com/wp/wp2823672.jpg)] bg-cover bg-center h-full w-screen">
         klkajsdlkj
     </div>
   )
