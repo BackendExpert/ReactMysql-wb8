b3ca363aed10c1ee5f12c9444f7e15c83374998d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-28 18:15:08 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/SignUp.jsx
+++ client/src/components/LoginSignUp/SignUp.jsx
@@ -1,7 +1,7 @@
 const SignUp = () => {
   return (
-    <div className="bg-[url(https://wallpapercave.com/wp/wp2823672.jpg)] bg-cover bg-center h-screen w-full">
-        <div className="">
+    <div className="bg-[url(https://wallpapercave.com/wp/wp2707503.jpg)] bg-cover bg-center h-screen w-full lg:pt-40">
+        <div className="border lg:mx-32">
             <div className="">
                 <div className="">
                     Welcome to UMS (User Management System)
