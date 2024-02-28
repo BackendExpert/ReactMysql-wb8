ca6656b5c790ee608d46b42c0d5dc80f34299eb7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-28 18:59:20 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/SignUp.jsx
+++ client/src/components/LoginSignUp/SignUp.jsx
@@ -8,6 +8,7 @@ const SignUp = () => {
             <div className="lg:flex justify-between ">
                 <div className="backdrop-blur-lg lg:h-[600px] h-[300px] lg:w-1/2 lg:px-20 px-10 lg:py-24 py-20">
                     <p className="lg:text-4xl text-2xl text-center text-white">Welcome to UMS <br />(User Management System)</p>
+                    <p className="text-4xl font-semibold mt-16 text-center">SignUp Here</p>
                 </div>
                 <div className="bg-white w-1/2">
                     <div className="">
