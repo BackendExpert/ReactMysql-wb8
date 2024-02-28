b052ebf98e7b48f926330e213df37e0d96f82919
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-28 07:23:13 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 import express from "express";
 import cors from 'cors';
 import mysql from "mysql2";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import nodemailer from 'nodemailer';
+import cookieParser from "cookie-parser";
+import multer from "multer";
+import path from "path";
 
 const app = express(); 
 app.use(cors());
