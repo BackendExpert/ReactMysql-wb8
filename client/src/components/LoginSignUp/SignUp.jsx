ef2cbdb7562cff35283250b84a6714ddd81bdb37
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-28 18:57:41 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/SignUp.jsx
+++ client/src/components/LoginSignUp/SignUp.jsx
@@ -1,25 +1,37 @@
-import { Link } from "react-router-dom"
-import IonIcons from "@reacticons/ionicons"
+import IonIons from '@reacticons/ionicons';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
   return (
-    <div className="bg-[url(https://wallpapercave.com/wp/wp2707504.jpg)] bg-cover bg-center h-screen w-full lg:pt-40 pt-20">
+    <div className="bg-[url(https://wallpapercave.com/wp/wp2707504.jpg)] bg-cover bg-center h-screen w-full lg:pt-40 pt-28">
         <div className="mx-12 lg:mx-32 rounded-xl">
             <div className="lg:flex justify-between ">
-                <div className="backdrop-blur-lg h-[600px] lg:w-1/2 lg:px-20 px-10 lg:py-24 py-20">
+                <div className="backdrop-blur-lg lg:h-[600px] h-[300px] lg:w-1/2 lg:px-20 px-10 lg:py-24 py-20">
                     <p className="lg:text-4xl text-2xl text-center text-white">Welcome to UMS <br />(User Management System)</p>
-                    <Link to={'/register'}>
-                        <div className="bg-transparent border w-1/2 ">
-                            <div className="flex">
-                                <p><IonIcons name="person-add"></IonIcons></p>
-                                <p className="pl-4">SignUp</p>
-                            </div>
-                        </div>
-                    </Link>
-                    
                 </div>
                 <div className="bg-white w-1/2">
-                    Welcome to UMS (User Management System)
+                    <div className="">
+                        <form className="py-20 px-16">
+                            <div className="py-2">
+                            <span><IonIons name='person' size='large'></IonIons></span>
+                                <label htmlFor="" className="text-xl pl-4">First Name: </label>
+                                <input type="text" className="w-full h-12 border my-2 pl-2" required placeholder="First Name" />
+                            </div>
+                            <div className="py-2">
+                                <span><IonIons name='at' size='large'></IonIons></span>
+                                <label htmlFor="" className="text-xl pl-4">Email: </label>
+                                <input type="email" className="w-full h-12 border my-2 pl-2" required placeholder="First Name" />
+                            </div>
+                            <div className="py-2">
+                                <span><IonIons name='key' size='large'></IonIons></span>
+                                <label htmlFor="" className="text-xl pl-4">Password: </label>
+                                <input type="password" className="w-full h-12 border my-2 pl-2" required placeholder="Password" />
+                            </div>
+                            <div className="py-2">
+                                <button type="submit" className="border border-cyan-600 w-1/2 text-cyan-600 rounded-lg h-12 duration-500 hover:bg-cyan-600 hover:text-white">SignUp</button>    
+                            </div>                          
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
