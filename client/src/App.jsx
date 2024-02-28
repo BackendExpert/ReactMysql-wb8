6c85f717851716f0565b7949b796a8e682419d29
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-28 17:59:03 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import "react-router-dom"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import SignIn from "./components/LoginSignUp/SignIn"
 
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={'/'} element={}
+        <Route path={'/'} element={<SignIn />} ></Route>
       </Routes>
     </BrowserRouter>
   )
