2ccd7f8d9012cca802f152d12654698febf34a75
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-28 07:00:34 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/server.js
+++ server/server.js
@@ -10,3 +10,26 @@ import path from "path";
 import { resourceLimits } from "worker_threads";
 
 
+const app = express(); 
+app.use(cors());
+app.use(express.json());
+
+const conn = mysql.createConnection
+({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: ''
+});
+
+//connect to database
+conn.connect((error) =>
+{
+  if(error) throw error
+  else 
+  console.log('Mysql Connected...');
+});
+
+app.listen(8081, () => {
+    console.log("Server running successfully on 8081");
+});
