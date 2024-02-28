9314d3eaf8b6fbe720bfcfb31a0321689842fac5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-28 18:11:11 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/SignUp.jsx
+++ client/src/components/LoginSignUp/SignUp.jsx
@@ -1,7 +1,16 @@
 const SignUp = () => {
   return (
     <div className="bg-[url(https://wallpapercave.com/wp/wp2823672.jpg)] bg-cover bg-center h-screen w-full">
-        
+        <div className="">
+            <div className="">
+                <div className="">
+                    Welcome to UMS (User Management System)
+                </div>
+                <div className="">
+                    Welcome to UMS (User Management System)
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
