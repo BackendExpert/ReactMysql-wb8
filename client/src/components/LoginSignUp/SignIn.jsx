76a8c1986ddab9aa1584fd9c444bbb7107e3cee7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-29 03:21:18 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/SignIn.jsx
+++ client/src/components/LoginSignUp/SignIn.jsx
@@ -4,7 +4,9 @@ const SignIn = () => {
     <div className="bg-[url(https://wallpapercave.com/wp/wp2707507.jpg)] bg-cover bg-center h-screen w-full lg:pt-40 pt-28">
         <div className="mx-12 lg:mx-32 rounded-xl">
         <div className="lg:flex justify-between ">
-            <div className=""></div>
+            <div className="bg-white w-1/2">
+              
+            </div>
             <div className="backdrop-blur-lg lg:h-[600px] h-[300px] lg:w-1/2 lg:px-20 px-10 lg:py-24 py-20">
               <p className="lg:text-4xl text-2xl text-center text-white">Welcome Back to UMS <br />(User Management System)</p>
               <p className="text-4xl font-semibold mt-16 text-center text-cyan-600">SignIn Here</p>
