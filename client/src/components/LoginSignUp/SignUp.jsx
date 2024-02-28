524f30af1ae56ec6fd86c1365a9b6f1aac10ac35
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-28 19:05:08 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/SignUp.jsx
+++ client/src/components/LoginSignUp/SignUp.jsx
@@ -8,30 +8,36 @@ const SignUp = () => {
             <div className="lg:flex justify-between ">
                 <div className="backdrop-blur-lg lg:h-[600px] h-[300px] lg:w-1/2 lg:px-20 px-10 lg:py-24 py-20">
                     <p className="lg:text-4xl text-2xl text-center text-white">Welcome to UMS <br />(User Management System)</p>
-                    <p className="text-4xl font-semibold mt-16 text-center">SignUp Here</p>
+                    <p className="text-4xl font-semibold mt-16 text-center text-cyan-600">SignUp Here</p>
                 </div>
                 <div className="bg-white w-1/2">
-                    <div className="">
-                        <form className="py-20 px-16">
+                    <div className="py-20 px-16">
+                        <form>
                             <div className="py-2">
                             <span><IonIons name='person' size='large'></IonIons></span>
                                 <label htmlFor="" className="text-xl pl-4">First Name: </label>
-                                <input type="text" className="w-full h-12 border my-2 pl-2" required placeholder="First Name" />
+                                <input type="text" className="w-full h-12 border my-2 pl-2 text-xl" required placeholder="First Name" />
                             </div>
                             <div className="py-2">
                                 <span><IonIons name='at' size='large'></IonIons></span>
                                 <label htmlFor="" className="text-xl pl-4">Email: </label>
-                                <input type="email" className="w-full h-12 border my-2 pl-2" required placeholder="First Name" />
+                                <input type="email" className="w-full h-12 border my-2 pl-2 text-xl" required placeholder="First Name" />
                             </div>
                             <div className="py-2">
                                 <span><IonIons name='key' size='large'></IonIons></span>
                                 <label htmlFor="" className="text-xl pl-4">Password: </label>
-                                <input type="password" className="w-full h-12 border my-2 pl-2" required placeholder="Password" />
+                                <input type="password" className="w-full h-12 border my-2 pl-2 text-xl" required placeholder="Password" />
                             </div>
                             <div className="py-2">
                                 <button type="submit" className="border border-cyan-600 w-1/2 text-cyan-600 rounded-lg h-12 duration-500 hover:bg-cyan-600 hover:text-white">SignUp</button>    
                             </div>                          
                         </form>
+
+                        <p>Already have an Account ? 
+                            <Link to={'/'}>
+                                <span className="px-2 text-cyan-600 duration hover:underline">SignIn</span>
+                            </Link>
+                        </p>
                     </div>
                 </div>
             </div>
