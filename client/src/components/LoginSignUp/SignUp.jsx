7223aa3ea7ee1976858e90fe9de9cc64604f271f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-28 18:30:21 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/SignUp.jsx
+++ client/src/components/LoginSignUp/SignUp.jsx
@@ -1,12 +1,22 @@
+import { Link } from "react-router-dom"
+import IonIcons from "@reacticons/ionicons"
+
 const SignUp = () => {
   return (
-    <div className="bg-[url(https://wallpapercave.com/wp/wp2707503.jpg)] bg-cover bg-center h-screen w-full lg:pt-40">
-        <div className="border lg:mx-32">
-            <div className="">
-                <div className="">
-                    Welcome to UMS (User Management System)
+    <div className="bg-[url(https://wallpapercave.com/wp/wp2707504.jpg)] bg-cover bg-center h-screen w-full lg:pt-40">
+        <div className="lg:mx-32 rounded-xl">
+            <div className="lg:flex justify-between ">
+                <div className="backdrop-blur-lg h-[600px] w-1/2 lg:px-20 lg:py-24">
+                    <p className="text-4xl text-center text-white">Welcome to UMS <br />(User Management System)</p>
+                    <Link to={'/register'}>
+                        <div className="flex bg-transparent">
+                            <p><IonIcons name="person-add"></IonIcons></p>
+                            <p className="pl-4">SignUp</p>
+                        </div>
+                    </Link>
+                    
                 </div>
-                <div className="">
+                <div className="bg-white w-1/2">
                     Welcome to UMS (User Management System)
                 </div>
             </div>
