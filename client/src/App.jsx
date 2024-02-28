7caedbbc5dec4f9738a5e5a49a716d8ef97451bf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-28 17:57:31 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,13 @@
+import "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} element={}
+      </Routes>
+    </BrowserRouter>
   )
 }
